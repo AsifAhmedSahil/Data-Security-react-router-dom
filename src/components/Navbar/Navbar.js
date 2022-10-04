5defadcb85a0ffc140bee41f479e9912ce560282
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-10-04 12:18:56 +0600

    data security home page and data recovery done

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { SiDatabricks } from 'react-icons/si'
+import { FaBars,FaTimes } from 'react-icons/fa'
+// import {Link} from 'react-router-dom'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => setNav(!nav)
+
+
+    return (
+        <div name='top' className='navbar'>
+            <div className="container">
+                <div className="logo">
+                    <SiDatabricks className='icon' />
+                    <h1>Secured</h1>
+                </div>
+
+                <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
+                    <li>Home</li>
+                    <li>Recovery </li>
+                    <li>Cloud</li>
+                    <li>Contact</li>
+                    <button>Sign in</button>
+                </ul>
+                <div className="hamburger" onClick={handleNav}>
+                    {!nav ? (<FaBars className='icon' />) : (<FaTimes className='icon' />)}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/data/Data.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Data.css'
+
+const Data = () => {
+  return (
+    <div className='data'>
+        <div className='container'>
+            <div className='content'>
+                <h2><span>Data</span> Recovery</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta cum quam ratione vitae ducimus enim rem cumque. Vitae, quod magni.</p>
+                <div>
+                    <button>Sign Up</button>
+                </div>
+
+            </div>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Data
